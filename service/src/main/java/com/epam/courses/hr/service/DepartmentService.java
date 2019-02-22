8709955104dc041f3b21ba2713062d732b60d9d9
Siarhei Berdachuk <Siarhei.Berdachuk@viacomcontractor.com>
2019-02-22 11:28:43 +0300

    remove unused imports

--- service/src/main/java/com/epam/courses/hr/service/DepartmentService.java
+++ service/src/main/java/com/epam/courses/hr/service/DepartmentService.java
@@ -2,7 +2,6 @@ package com.epam.courses.hr.service;
 
 import com.epam.courses.hr.model.Department;
 
-import java.util.Optional;
 import java.util.stream.Stream;
 
 public interface DepartmentService {
