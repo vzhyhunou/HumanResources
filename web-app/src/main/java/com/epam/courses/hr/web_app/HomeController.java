77a5fb6d263bd815083287d39857a4932990629f
Siarhei Berdachuk <Siarhei.Berdachuk@viacomcontractor.com>
2019-02-27 12:42:26 +0300

    menu navigation

--- web-app/src/main/java/com/epam/courses/hr/web_app/HomeController.java
+++ web-app/src/main/java/com/epam/courses/hr/web_app/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
      */
     @GetMapping(value = "/")
     public String defaultPageRedirect() {
-        return "redirect:hello";
+        return "redirect:departments";
     }
 }
 
